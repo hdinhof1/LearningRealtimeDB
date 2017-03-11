95c0687d1b36f78491d47774edf82da267701aca
hdinhof1 <hdinhof1@binghamton.edu>
2017-03-11 14:23:23 -0500

    Initial Commit

---
+++ LearningRealtimeDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningRealtimeDB
+//
+//  Created by Henry Dinhofer on 3/11/17.
+//  Copyright © 2017 Henry Dinhofer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
